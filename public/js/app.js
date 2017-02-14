3ee2480a0ba6fb598e8e4d6a927ce0450bda885a
Matthew Morais <moraismatthew@yahoo.com>
2017-02-13 19:50:30 -0500

    Added the base html to set up huttons, then added some css to inline-block which did not work, lastly added a base setup variable called Counter to prepare for the actual functionality of the counter app.

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+var Counter = {
+    init: function(){
+        Counter.cacheDOM();
+        Counter.addEventListener();
+        Counter.render();
+    },
+    cacheDOM: function(){
+
+    },
+    addEventListener: function(){},
+    render: function(){}
+
+};
+
+Counter.init();
